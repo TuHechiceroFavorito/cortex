b191317d52b65f677e53be40be0b132b1e2d36b6
Alberto Aróstegui <aarosteguig@gmail.com>
2021-04-21 20:17:50 +0200

    1

--- tests/elec.py
+++ tests/elec.py
@@ -11,8 +11,21 @@ class Led:
         self.estado = 0
 
 if __name__ == '__name__':
-    l1 = Leds()
+    l1 = Led()
 
     l1.ton()
 
     print(l1.estado)
+
+class sensor:
+    def __init__(self):
+        self.deteccion = 0
+
+    def detectar(self):
+        self.deteccion = 1 #Aquí que llegue un comando que sea generado por la detección física
+        self.marcar()
+        self.deteccion = 0 #Asumo que buscamos un pulso no un switch
+
+    def marcar(self):
+
+        return(self.deteccion)
