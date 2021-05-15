c1f1e0f9a4e37ba3cae89f7b3baaf8e7845377cd
TuHechiceroFavorito <daniel.alvarez.car@gmail.com>
2021-05-15 04:18:08 +0200

    Added Telegram interface

---
+++ tests/tg.py
@@ -0,0 +1,38 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+import os
+
+token = os.environ.get("cortex_tg")
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+def test(update, context):
+    logging.info('Request for testing sent')
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Alberto chupapollas")
+
+def on(update, context):
+    logging.info('Message to turn on the LED sent')
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Turning LED on")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="LED on")
+
+def off(update, context):
+    logging.info('Message to turn off the LED sent')
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Turning LED off")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="LED off")
+
+def main():
+    updater = Updater(token=token, use_context=True)
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('test', test))
+    dp.add_handler(CommandHandler('on', on))
+    dp.add_handler(CommandHandler('off', off))
+
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
