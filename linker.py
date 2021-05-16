7c7d83b011405daa5fd61ea7b228f350e402cd4f
TuHechiceroFavorito <daniel.alvarez.car@gmail.com>
2021-05-16 02:34:42 +0200

    First attempt to create a cortex

---
+++ linker.py
@@ -0,0 +1,28 @@
+from time import sleep
+import concurrent.futures as cf
+
+def name(name='asd'):
+    for i in range(5):
+        print(f'My name is {name}')
+        sleep(2)
+
+def age(age=0):
+    for i in range(5):
+        print(f"I'm {age} years old")
+        sleep(2)
+
+class CreateInterface:
+    def __init__(self, funk, *args):
+        self.funk = funk
+        self.args = list(args)
+
+    def startInterface(self):
+        with cf.ThreadPoolExecutor() as ex:
+            ex.submit(self.funk)
+
+nombre = CreateInterface(name)
+edad = CreateInterface(age)
+
+nombre.startInterface()
+edad.startInterface()
+
